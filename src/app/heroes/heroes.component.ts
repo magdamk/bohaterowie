c6e0f28380e6c4ec123f3f84ade577fa67969ce5
magdamk <magdamroziuk@wp.pl>
2022-11-08 13:08:21 +0100

    messages added

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -12,15 +13,17 @@ export class HeroesComponent implements OnInit {
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`Heroes: wybrano bohatera id=${hero.id}`);
   }
 
   onUnSelect(): void {
     this.selectedHero = undefined;
+    this.messageService.add('Heroes: wyczyszczono wybór');
   }
 
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   getHeroes(): void {
    this.heroService.getHeroes().subscribe((heroes): Hero[] => {
